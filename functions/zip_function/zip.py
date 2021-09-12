be4ca840dffd491297371b5482618b0a094f7022
mlobf <marcoslemeborbafilho@gmail.com>
2021-09-12 18:42:34 -0300

    This is a simple test using vim to create a commit

--- functions/zip_function/zip.py
+++ functions/zip_function/zip.py
@@ -1,18 +1,24 @@
-
-
 """
 -The zip functions takes some iterators and
 zips Them on Tuples.
 - Used to parallel iterations
 - Retuns a zip object which is an iterators of tuples.
+
+the zip function takes the len of the shortest list
+    ands used it as main path to zip to the other list.
 """
 
-countries = ["Ecuador", "Peru", "Colombia", "USA", "Brazil"]
+countries = ["Ecuador", "Peru", "Colombia", "USA", "Brazil", "Chile"]
 capitals = ["Quito", "Lima", "Bogota", "Washintown DC", "Brasila"]
 
 
 countries_capitals = zip(countries, capitals)
 
-print(countries_capitals.__next__())
-print('-------')
-print(countries_capitals.__next__())
+print(type(countries_capitals))
+
+for countries, capitals in countries_capitals:
+
+    print(countries, capitals)
+
+
+print(type(list(countries_capitals)))
