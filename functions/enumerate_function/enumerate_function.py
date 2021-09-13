3ef633cb93213c66b2daeae1e739da023283cdcf
mlobf <marcoslemeborbafilho@gmail.com>
2021-09-13 16:27:48 -0300

    commit add enumerate

---
+++ functions/enumerate_function/enumerate_function.py
@@ -0,0 +1,16 @@
+"""
+    Add a counter at a iterable and return the this
+    counter .
+"""
+
+fruits = ["apple", "pineapple", "lemon", "watermelon", "grapes"]
+
+enumerate_list = enumerate(fruits)
+
+
+for index, element in enumerate_list:
+    filename = f"file{index}.jpg"
+
+    print(filename)
+
+
