8a16feccfc4953f2a20cad927d832025bcf0cada
mlobf <marcoslemeborbafilho@gmail.com>
2021-09-13 14:04:30 -0300

    Sum like in school challenge

---
+++ functions/ChallengeOne/sum_like_schooll.py
@@ -0,0 +1,22 @@
+from itertools import zip_longest
+
+
+def sum_like_in_school(number_1, number_2, base=10):
+    digits_zipped = zip_longest(
+        reversed(str(number_1)), reversed(str(number_2)), fillvalue=0)
+    carry = 0
+    result = ""
+    for digit_1, digit_2 in digits_zipped:
+        sum_of_two_digits = int(digit_1) + int(digit_2) + carry
+        if sum_of_two_digits >= base:
+            carry = 1
+            result = str(sum_of_two_digits % base) + result
+        else:
+            carry = 0
+            result = str(sum_of_two_digits) + result
+    return int(result)
+
+
+print(
+    sum_like_in_school(1564, 587, 10)
+)
---
+++ functions/longest_zip_function/long_zip_function.py
@@ -0,0 +1,23 @@
+from itertools import zip_longest
+
+
+"""
+    This one takes the longest iterable - list, tuple, dict, etc
+    instead of the shortest
+"""
+
+countries = ("Ecuador", "Peru", "Colombia", "USA", "Brazil", "Chile", "Bolivia")
+
+capitals = ("Quito", "Lima", "Bogota", "Washintown DC", "Brasila", "Santiago")
+
+countries_capitals = zip_longest(countries, capitals, fillvalue="My Default City")
+
+print("------------------")
+
+print(type(countries_capitals))
+
+print("------------------")
+print(list(countries_capitals))
+
+print("------------------")
+print(type(list(countries_capitals)))
