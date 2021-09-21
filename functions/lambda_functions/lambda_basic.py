db3d7e846c3ef7eb64dfafa90fdbf062aa0223f6
mlobf <marcoslemeborbafilho@gmail.com>
2021-09-20 22:40:40 -0300

    Review some comprehentions and lambda functions

---
+++ functions/lambda_functions/lambda_basic.py
@@ -0,0 +1,13 @@
+# l = lambda x:x
+# def l(x): return x
+
+
+add = lambda x: x + 5
+less = lambda x: x - 5
+mult = lambda x: x * 5
+pot = lambda x: x ** 5
+
+print(add(4))
+print(less(4))
+print(mult(4))
+print(pot(4))
---
+++ functions/lambda_functions/lambda_iterator.py
@@ -0,0 +1,14 @@
+'''
+print(lambda x: x for x in range(10))
+'''
+
+#print((lambda x, y: x * y)(int(5), int(7)))
+
+# print((lambda x, y=3, z=5: x*y*z)(7))
+
+# lambda arguments: expression
+
+
+print(
+    lambda x
+)
