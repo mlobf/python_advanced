0635ffa210438c7078ecea14727d611bb9882fa7
mlobf <marcoslemeborbafilho@gmail.com>
2022-04-23 23:49:18 -0300

    Testing Solus Gnome

--- generators/map.py
+++ generators/map.py
@@ -4,7 +4,7 @@ import sys
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # x = range(1, 99999999)
 
-y = map(lambda i: i ** 2, x)
+y = map(lambda i: i**2, x)
 
 print(sys.getsizeof(y))
 # print(sys.getsizeof(list(y)))
